5bf1b1077961241f2af8baffa397cd85f9b7e404
aishwaryagodam <aishwaryagodam137@gmail.com>
2020-09-30 19:45:58 +0530

    initial project setup

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view("home");
+	}
+
+	public function about()
+	{
+		$this->load->view("about");
+	}
+	
+	public function services()
+	{
+		$this->load->view("services");
+	}
+
+	public function contact()
+	{
+		$this->load->view("contact");
+	}
+}
---
+++ application/views/menubar.php
@@ -0,0 +1,43 @@
+<section class=" w3l-header-4 header-sticky">
+    <header class="absolute-top">
+        <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <h1><a class="navbar-brand" href="index.html"> <!--<span class="fa fa-line-chart" aria-hidden="true"></span> -->
+            parlour
+            </a></h1>
+            <button class="navbar-toggler bg-gradient collapsed" type="button" data-toggle="collapse"
+                data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="fa icon-expand fa-bars"></span>
+				<span class="fa icon-close fa-times"></span>
+            </button>
+      
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo site_url();?>">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url();?>welcome/about">About</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url();?>welcome/services">Services</a>
+                    </li> 
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url();?>welcome/contact">Contact</a>
+                    </li>
+                </ul>
+                  <form action="#" method="post">
+                    <div class="search-button d-flex">
+                    <input class="form-control search" type="search" placeholder="Search here..." aria-label="Search" required="">
+                    <button class="btn buttn-subscribe" type="submit"><span class="fa fa-search" aria-hidden="true"></span></button>
+                </div>  
+                </form>
+            </div>
+        </div>
+
+        </nav>
+    </div>
+      </header>
+</section>
